dc5d98a9fc04777d52413185b97955912dfe4805
IT24102268 <it24102268@my.sliit.lk>
2025-10-08 11:36:39 +0530

    IT24102268

---
+++ Lab Sheet 09/IT24102268.R
@@ -0,0 +1,13 @@
+getwd()
+setwd("D:\\Sliit\\y2s1\\Ps\\lab\\LAB9")
+getwd()
+
+#Exercise
+# 1)
+#i)
+baking_time <- rnorm(25, mean = 45, sd = 2)
+
+#ii)
+t.test(baking_time, mu = 46, alternative = "less")
+
+
