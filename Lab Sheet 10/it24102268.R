51734ab50aade29e105a1a1896397d0643b732a4
Minaz <minoshaluthge@gmail.com>
2025-10-22 23:36:47 +0530

    lab10

--- Lab Sheet 10/it24102268.R
+++ Lab Sheet 10/it24102268.R
@@ -1,36 +1,33 @@
+# IT2120 - Lab Sheet 10
 
-setwd("C:\Users\MINAZ \Desktop \PS Lab10 it24102268
 
-# Question 01
-# Part 1
+# Set working directory
+setwd("C:\\Users\\dulsh\\Desktop\\PS Lab10 it24102268")
+
+# Question 1: Customer Distribution Test
+cat("=== Question 1 ===\n")
 observed <- c(55, 62, 43, 46, 50)
 prob <- c(0.2, 0.2, 0.2, 0.2, 0.2)
-chisq.test(x = observed, p = prob)
+chisq_result1 <- chisq.test(x = observed, p = prob)
+print(chisq_result1)
+cat("Conclusion: Since p-value > 0.05, we do not reject H0.\n")
+cat("Customers appear to arrive with equal probability each weekday.\n\n")
 
-# Part 2
-# P-value = 0.351 > 0.05 - Do not reject H0
+# Question 2: House Tasks Association Test
+cat("=== Question 2 ===\n")
+file_path <- "http://www.sthda.com/sthda/RDoc/data/housetasks.txt"
+housetasks <- read.delim(file_path, row.names = 1)
+chisq_result2 <- chisq.test(housetasks)
+print(chisq_result2)
+cat("Conclusion: Since p-value < 0.05, we reject H0.\n")
+cat("There is significant association between tasks and distribution.\n\n")
 
-# Question 02
-# Part 1
-file path <- "http://www.sthda.com/sthda/RDoc/data/housetasks.txt"
-housetasks <- read.delim(file path, row.names - 1)
-housetasks
+# Exercise: Snack Type Preference
+cat("=== Exercise ===\n")
+snack_observed <- c(120, 95, 85, 100)
+snack_prob <- c(0.25, 0.25, 0.25, 0.25)
+snack_chisq <- chisq.test(x = snack_observed, p = snack_prob)
+print(snack_chisq)
+cat("Conclusion: Since p-value > 0.05, we do not reject H0.\n")
+cat("No evidence against equal preference for snack types.\n")
 
-# Part 2
-chisq <- chisq.test(housetasks)
-chisq
-# P-value = 2.2e-16 < 0.05 - Reject H0
-
-# Exercise
-# Part i: Hypotheses
-# HO: pA = pB = pC = pD = 0.25
-# Hl: At least one probability is different
-
-# Part ii: Chi-squared test
-observed <- c(120, 95, 85, 100)
-prob <- c(0.25, 0.25, 0.25, 0.25)
-result <- chisq. test(x - observed, p = prob)
-result
-
-# Part iii: Conclusion
-# 0.08966 > 0.05 - do not reject H0
